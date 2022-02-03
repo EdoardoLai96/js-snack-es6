7348628d3c404cb7ccd7f9bdffea1eff9d85a510
Edoardo Lai <edoardolai96@gmail.com>
2022-02-03 15:49:50 +0100

    Esercizio snack 1

---
+++ Snack 1/main.js
@@ -0,0 +1,30 @@
+const biciclette = [
+
+    {   
+       nome: "Bici 1",
+       peso: 10 
+    },
+
+    {   
+       nome: "Bici 2",
+       peso: 12 
+    },
+
+    {   
+       nome: "Bici 3",
+       peso: 14 
+    },
+
+    {   
+       nome: "Bici 4",
+       peso: 16 
+    },
+
+
+];
+
+
+const [bicicletta_uno, bicicletta_due, bicicletta_tre, bicicletta_quattro] = biciclette;
+
+console.log(bicicletta_uno);
+
